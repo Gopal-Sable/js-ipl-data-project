f974bef2a6c67242b82597f5785f9bbd9dd70cd1
Gopal Sable <sablegopal123@gmail.com>
2025-02-09 15:39:14 +0530

    man of the season wrapped it function

--- src/server/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-if (!strikeRate[key][ball.batsman]) {
-      //   strikeRate[key][ball.batsman] = { "runs": 0, "total-balls": 0 };
-      // }
-      // strikeRate[key][ball.batsman].runs += parseInt(ball.batsman_runs);
-      
-      // if (ball.extra_runs == "0") {
-      //   strikeRate[key][ball.batsman].total-balls++;
-      // }
