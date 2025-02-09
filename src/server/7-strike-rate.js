33f5ec5fee76edc576ae036f1884b1ec201d9465
Gopal Sable <sablegopal123@gmail.com>
2025-02-09 16:08:53 +0530

    strike-rate wraped into function

--- src/server/7-strike-rate.js
+++ src/server/7-strike-rate.js
@@ -4,42 +4,43 @@ import readFile from "./fileReader.js";
 const matches = await readFile("./src/data/matches.json");
 const deliveries = await readFile("./src/data/deliveries.json");
 
-const result = matches.reduce((seasonMatchIds, match) => {
-  if (!seasonMatchIds[match.season]) {
-    seasonMatchIds[match.season] = [match.id];
-  } else {
-    seasonMatchIds[match.season] = [...seasonMatchIds[match.season], match.id];
-  }
-  return seasonMatchIds;
-}, {});
+function strikRatesOfSeasons(matches, deliveries) {
+  const matchIds = matches.reduce((seasonMatchIds, match) => {
+    if (!seasonMatchIds[match.season]) {
+      seasonMatchIds[match.season] = [match.id];
+    } else {
+      seasonMatchIds[match.season].push(match.id)
+    }
+    return seasonMatchIds;
+  }, {});
 
-// console.log(result);
+  // console.log(matchIds);
 
-const strikeRates = {};
-deliveries.forEach((ball) => {
-  for (const key in result) {
-    if (result[key].includes(ball.match_id)) {
-      if (!strikeRates[key]) {
-        strikeRates[key] = {};
-      }
-      if (!strikeRates[key][ball.batsman]) {
-        strikeRates[key][ball.batsman] = { runs: 0, totalballs: 0 };
-      }
-      strikeRates[key][ball.batsman].runs += parseInt(ball.batsman_runs);
+  const strikeRates = {};
+  deliveries.forEach(({match_id,batsman,batsman_runs,extra_runs}) => {
+    for (const key in matchIds) {
+      if (matchIds[key].includes(match_id)) {
+        if (!strikeRates[key]) {
+          strikeRates[key] = {};
+        }
+        if (!strikeRates[key][batsman]) {
+          strikeRates[key][batsman] = { runs: 0, totalballs: 0 };
+        }
+        strikeRates[key][batsman].runs += parseInt(batsman_runs);
 
-      if (ball.extra_runs == "0") {
-        strikeRates[key][ball.batsman].totalballs++;
+        if (extra_runs == "0") {
+          strikeRates[key][batsman].totalballs++;
+        }
+        
       }
-      strikeRates[key][ball.batsman].strikRate = (
-        (strikeRates[key][ball.batsman].runs * 100) /
-        strikeRates[key][ball.batsman].totalballs
-      ).toFixed(2);
+    }
+  });
+  for (const season in strikeRates) {
+    for (const batsman in strikeRates[season]) {
+      const { runs, totalballs } = strikeRates[season][batsman];
+      strikeRates[season][batsman] = +(runs / totalballs * 100).toFixed(2) ;
     }
   }
-});
-console.log(strikeRates);
-
-// {
-//     2015:[1,2,3],
-//     2016:[5,6,7]
-// }
+  return strikeRates;
+}
+// console.log(strikRatesOfSeasons(matches,deliveries));
