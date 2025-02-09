467f84879eda6625b6424173bafdb92a543c0a70
Gopal Sable <sablegopal123@gmail.com>
2025-02-09 15:24:41 +0530

    wrapping into function

--- src/server/4-top-10-economy.js
+++ src/server/4-top-10-economy.js
@@ -4,36 +4,41 @@ import readFile from "./fileReader.js";
 const matches = await readFile("./src/data/matches.json");
 const deliveries = await readFile("./src/data/deliveries.json");
 
-const matchesInYear=(year)=>{
-   return matches.filter((match)=>{
-     return  match.season==year
-    }).map((match)=>match.id)
-}
-const matchesOfYear= matchesInYear(2015);
+function getTopEconomicalBowlers(year, topN = 10) {
+  const matchesOfYear = matches
+    .filter(({ season }) => season == year)
+    .map(({ id }) => id);
 
-let data={}
-deliveries.forEach(over => {
-    if(matchesOfYear.includes(over.match_id)){
-        let runs=parseInt(over.wide_runs )+ parseInt(over.noball_runs) + parseInt(over.batsman_runs)
-    //    let overs= ((Total Balls - No Balls - Wide Balls) / 6)
+  const data = {};
+  deliveries.forEach(
+    ({ match_id, bowler, wide_runs, noball_runs, batsman_runs }) => {
+      if (matchesOfYear.includes(match_id)) {
+        const runs =
+          Number(wide_runs) + Number(noball_runs) + Number(batsman_runs);
 
-        if (!data[over.bowler]) {
-            data[over.bowler]={}
-            data[over.bowler].total_runs=0
-            data[over.bowler].total_balls=0
+        if (!data[bowler]) {
+          data[bowler] = { total_runs: 0, total_balls: 0 };
         }
-        data[over.bowler].total_runs +=runs;
-        if (over.wide_runs==="0" && over.noball_runs==="0") {
-            data[over.bowler].total_balls++;
+
+        data[bowler].total_runs += runs;
+
+        if (wide_runs === "0" && noball_runs === "0") {
+          data[bowler].total_balls++;
         }
-        data[over.bowler].economy=(data[over.bowler].total_runs/data[over.bowler].total_balls)*6
+      }
     }
-});
+  );
 
+  const economyData = Object.entries(data)
+    .map(([bowler, stats]) => ({
+      bowler,
+      economy:
+        Math.round((stats.total_runs / (stats.total_balls / 6)) * 100) / 100,
+    }))
+    .sort((a, b) => a.economy - b.economy)
+    .slice(0, topN);
 
+  return economyData;
+}
 
-console.log(Object.entries(data).sort((a,b)=>{
-    return a[1].economy - b[1].economy})
-    .map(data=>{
-        return {[data[0]]:Math.round(data[1].economy * 100) / 100}
-    }).slice(0,10))
+console.log(getTopEconomicalBowlers(2015));
