9dfb85474dffebd761134b34381444761c0841c6
Gopal Sable <sablegopal123@gmail.com>
2025-02-12 11:53:19 +0530

    refactored code

--- src/server/1-matches-per-year.js
+++ src/server/1-matches-per-year.js
@@ -5,10 +5,13 @@ const matches = readFile("./src/data/matches.json");
 // Number of matches played per year for all the years in IPL.
 function matchesPerYear(matches) {
   // console.log("Output generated");
-  return matches.reduce((matchYears, {season}) => { 
-      matchYears[season]=(matchYears[season] || 0)+1;
+  return matches.reduce((matchYears, { season }) => {
+    matchYears[season] = (matchYears[season] || 0) + 1;
     return matchYears;
   }, {});
 }
 
-writeFile("./src/public/output/1-matches-per-year.json", matchesPerYear(matches));
+writeFile(
+  "./src/public/output/1-matches-per-year.json",
+  matchesPerYear(matches)
+);
