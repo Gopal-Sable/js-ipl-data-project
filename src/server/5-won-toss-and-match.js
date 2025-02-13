8a0f8e34a35687345659e5729b105fab513769d6
Gopal Sable <sablegopal123@gmail.com>
2025-02-13 17:39:13 +0530

    Converted win toss and match function to loops

--- src/server/5-won-toss-and-match.js
+++ src/server/5-won-toss-and-match.js
@@ -5,12 +5,14 @@ const deliveries = readFile("./src/data/deliveries.json");
 // Find the number of times each team won the toss and also won the match
 
 const matchesWin = (matches) => {
-  return matches.reduce((results, match) => {
-    if (match.toss_winner == match.winner) {
-      results[match.winner] = (results[match.winner] || 0) + 1;
+  const matchesResults = {};
+  for (let i = 0; i < matches.length; i++) {
+    const { winner, toss_winner } = matches[i];
+    if (toss_winner == winner) {
+      matchesResults[winner] = (matchesResults[winner] || 0) + 1;
     }
-    return results;
-  }, {});
+  }
+  return matchesResults;
 };
 
 writeFile("./src/public/output/5-won-toss-and-match.json", matchesWin(matches));
