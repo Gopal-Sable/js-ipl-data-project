5c59e18a8c5f9abd9e0f803df8f6a404a745287d
Gopal Sable <sablegopal123@gmail.com>
2025-02-13 16:28:42 +0530

    Converted getMatchIds helper function to loops

--- src/server/3-extra-runs.js
+++ src/server/3-extra-runs.js
@@ -6,18 +6,20 @@ const deliveries = readFile("./src/data/deliveries.json");
 // 3 Extra runs conceded per team in the year 2016
 
 function extraRunsConceded(year) {
-  const seasonMatchIds = getMatchIds(year);
+  // const seasonMatchIds = getMatchIds(year);
 
-  const extras = deliveries.reduce(
-    (extra, { match_id, bowling_team, extra_runs }) => {
-      if (seasonMatchIds.includes(match_id)) {
-        extra[bowling_team] = (extra[bowling_team] || 0) + parseInt(extra_runs);
-      }
-      return extra;
-    },
-    {}
-  );
-  return extras;
+  // const extras = deliveries.reduce(
+  //   (extra, { match_id, bowling_team, extra_runs }) => {
+  //     if (seasonMatchIds.includes(match_id)) {
+  //       extra[bowling_team] = (extra[bowling_team] || 0) + parseInt(extra_runs);
+  //     }
+  //     return extra;
+  //   },
+  //   {}
+  // );
+  // return extras;
+
+  
 }
 
 const result = extraRunsConceded(2016);
--- src/utility/getMatchIds.js
+++ src/utility/getMatchIds.js
@@ -2,10 +2,15 @@ import readFile from "../utility/fileReader.js";
 const matches = readFile("./src/data/matches.json");
 
 export default function getMatchIds(year) {
-  return matches.reduce((ids, { id, season }) => {
-    if (season == year) {
-      ids.push(id);
-    }
-    return ids;
-  }, []);
+  let ids=[];
+  for (let i = 0; i < matches.length; i++) {
+    let season=matches[i].season;
+    let id=matches[i].id;
+    if (season==year) {
+      ids.push(id)
+    }    
+  }
+  return ids;
 }
+
+console.log(getMatchIds(2015));
